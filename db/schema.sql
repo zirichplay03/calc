06389a014c6994df58843e47d4a75963eeddc2d9
zirichplay03 <k34kirill@mail.ru>
2025-02-20 18:31:32 +0700

    поправки скрипта бд

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,19 @@
 -- Создание таблицы пользователей
 CREATE TABLE IF NOT EXISTS users (
-                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                     username TEXT NOT NULL UNIQUE,
-                                     password TEXT NOT NULL,
-                                     balance REAL DEFAULT 0.0
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+balance REAL DEFAULT 0.0
 );
 
 -- Создание таблицы журнала действий
 CREATE TABLE IF NOT EXISTS logs (
-                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    username TEXT NOT NULL,
-                                    action TEXT NOT NULL,
-                                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL,
+action TEXT NOT NULL,
+timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- Пример добавления пользователей
+-- Добавления пользователей
 INSERT OR IGNORE INTO users (username, password, balance) VALUES ('user1', 'password1', 100.0);
 INSERT OR IGNORE INTO users (username, password, balance) VALUES ('user2', 'password2', 50.0);
